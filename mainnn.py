103ea327a2411adc1b33c97c2fbcab6286cfcc76
TheC0ala <76583019+TheC0ala@users.noreply.github.com>
2021-01-15 13:06:29 +0400

    Update and rename mainn.py to mainnn.py

--- mainnn.py
+++ mainnn.py
@@ -9,5 +9,5 @@ with TelegramClient(
 ) as client:
     session_str = client.session.save()
     s_m = client.send_message("me", session_str)
-    print('Kayıtlı mesajlara baxaraq String nizi görə bilərsiz.')
-    print('Daha sonra isə Herokuya gedərək Deploya davam edin.\n\n@UserBotTG')
+    print('String Session kayıtlı mesajlara qeyd edildi.')
+    print('Herokuya keçerək deploya davam edin.')
